404374cab4cf00553871c5ae4f5094016b90836c
Cristiano Castro <cristianodmtsb@gmail.com>
2019-08-11 14:54:07 -0300

    Socket Io Client install

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from "react";
 import api from "../services/api";
+import io from "socket.io-client";
 import Logo from "../assets/logo.svg";
 import Like from "../assets/like.svg";
 import Dislike from "../assets/dislike.svg";
+import ItsaMatch from "../assets/itsamatch.png";
 
 import "./main.css";
 
 export default function Main({ match }) {
   const [users, setUsers] = useState([]);
+  const [matchDev, setMatchDev] = useState(null);
 
   useEffect(() => {
     async function loadUsers() {
@@ -17,6 +20,16 @@ export default function Main({ match }) {
       setUsers(response.data);
     }
     loadUsers();
+  }, [match.params.id]);
+
+  useEffect(() => {
+    const socket = io("http://localhost:3333", {
+      query: { user: match.params.id }
+    });
+
+    socket.on("match", dev => {
+      setMatchDev(dev);
+    });
   }, [match.params.id]);
 
   async function handlelike(id) {
@@ -60,6 +73,17 @@ export default function Main({ match }) {
           <h2>Lista Vazia</h2>
         )}
       </ul>
+      {matchDev && (
+        <div className="match-container">
+          <img src={ItsaMatch} alt="It's a Match!" />
+          <img className="avatar" src={matchDev.avatar} alt={matchDev.name} />
+          <strong>{matchDev.name}</strong>
+          <p>{matchDev.bio}</p>
+          <button type="button" onClick={() => setMatchDev(null)}>
+            Fechar
+          </button>
+        </div>
+      )}
     </div>
   );
 }
